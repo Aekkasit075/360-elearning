a5cffff60d5f10f193cf5c8990946453b3ad6032
aekkasit <aekkasit.sak@student.mahidol.edu>
2026-07-28 18:16:28 +0700

    footer tab

---
+++ assets/js/layout.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+
+    fetch("components/header/header.html")
+
+    .then(response => response.text())
+
+    .then(data => {
+
+        document.getElementById("header").innerHTML = data;
+
+    });
+
+
+
+    fetch("components/footer/footer.html")
+
+    .then(response => response.text())
+
+    .then(data => {
+
+        document.getElementById("footer").innerHTML = data;
+
+    });
+
+
+});
