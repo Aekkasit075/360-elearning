d798a9a9c9c190d8a5d0aeed09491203798aea5b
aekkasit <aekkasit.sak@student.mahidol.edu>
2026-07-31 10:10:56 +0700

    footer background

---
+++ assets/js/scroll-animation.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+
+    const reveals = document.querySelectorAll(".reveal");
+
+
+    const observer = new IntersectionObserver(
+        (entries)=>{
+
+
+            entries.forEach(entry=>{
+
+
+                if(entry.isIntersecting){
+
+
+                    entry.target.classList.add("show");
+
+
+                }
+
+
+            });
+
+
+        },
+        {
+            threshold:0.15
+        }
+
+    );
+
+
+    reveals.forEach((element)=>{
+
+        observer.observe(element);
+
+    });
+
+
+});
