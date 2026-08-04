1331bb8d08c16f7074d4001b2488422a5528046a
aekkasit <aekkasit.sak@student.mahidol.edu>
2026-08-04 12:21:50 +0700

    short-course banner

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -2,63 +2,170 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
     fetch("components/header/header.html")
-
     .then(response => response.text())
-
     .then(data => {
-
         document.getElementById("header").innerHTML = data;
-
     });
 
 
-    fetch("components/home/hero/hero.html")
-    .then(response => response.text())
-    .then(data => {
+    // Home
+    if(document.getElementById("hero")){
 
-        document.getElementById("hero").innerHTML = data;
+        fetch("components/home/hero/hero.html")
+        .then(response => response.text())
+        .then(data=>{
+            document.getElementById("hero").innerHTML=data;
+        });
 
-    });
-
-
-    fetch("components/home/ourcourse/ourcourse.html")
-    .then(response => response.text())
-    .then(data => {
-
-        document.getElementById("ourcourse").innerHTML = data;
-
-    });
+    }
 
 
-    fetch("components/home/recommended/recommended.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("recommended").innerHTML = data;
-    });
+    if(document.getElementById("ourcourse")){
+
+        fetch("components/home/ourcourse/ourcourse.html")
+        .then(response=>response.text())
+        .then(data=>{
+            document.getElementById("ourcourse").innerHTML=data;
+        });
+
+    }
 
 
-    fetch("components/home/news/news.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("news").innerHTML = data;
-    });
+    if(document.getElementById("recommended")){
+
+        fetch("components/home/recommended/recommended.html")
+        .then(response=>response.text())
+        .then(data=>{
+            document.getElementById("recommended").innerHTML=data;
+        });
+
+    }
 
 
-    fetch("components/home/reviews/reviews.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("reviews").innerHTML = data;
-    });
+    if(document.getElementById("news")){
+
+        fetch("components/home/news/news.html")
+        .then(response=>response.text())
+        .then(data=>{
+            document.getElementById("news").innerHTML=data;
+        });
+
+    }
+
+
+    if(document.getElementById("reviews")){
+
+        fetch("components/home/reviews/reviews.html")
+        .then(response=>response.text())
+        .then(data=>{
+            document.getElementById("reviews").innerHTML=data;
+        });
+
+    }
+
+
+    // ===== Short Course =====
+
+    if(document.getElementById("banner_overview")){
+
+        fetch("components/short-course/banner_overview.html")
+        .then(response => response.text())
+        .then(data =>{
+
+            document.getElementById("banner_overview").innerHTML = data;
+
+            // Swiper
+            if(document.querySelector(".highlightSwiper")){
+
+                new Swiper(".highlightSwiper",{
+
+
+                    loop:false,
+
+
+                    centeredSlides:true,
+
+
+                    slidesPerView:3,
+
+
+                    spaceBetween:60,
+
+
+                    speed:600,
+
+
+                    grabCursor:true,
+
+
+
+                    navigation:{
+
+                        nextEl:".swiper-button-next",
+
+                        prevEl:".swiper-button-prev"
+
+                    },
+
+
+
+                    pagination:{
+
+                        el:".swiper-pagination",
+
+                        clickable:true
+
+                    },
+
+
+
+                    on:{
+
+
+                        slideChangeTransitionStart:function(){
+
+
+                            document
+                            .querySelectorAll(".highlight-card")
+                            .forEach(card=>{
+
+                                card.classList.remove("active");
+
+                            });
+
+
+
+                            let active =
+                            this.slides[this.activeIndex]
+                            .querySelector(".highlight-card");
+
+
+                            if(active){
+
+                                active.classList.add("active");
+
+                            }
+
+
+                        }
+
+
+                    }
+
+
+                });
+
+            }
+
+        });
+
+    }
 
 
     fetch("components/footer/footer.html")
-
     .then(response => response.text())
-
     .then(data => {
-
         document.getElementById("footer").innerHTML = data;
-
     });
 
 
@@ -71,8 +178,6 @@ document.addEventListener("DOMContentLoaded", function(){
             data
         );
 
-
-        // แจ้งว่า report component พร้อมแล้ว
         document.dispatchEvent(
             new Event("reportProblemLoaded")
         );
