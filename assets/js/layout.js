8dad3c7186b5da7b601e6b0aa7bb26aa4382ccde
aekkasit <aekkasit.sak@student.mahidol.edu>
2026-08-05 13:51:10 +0700

    course detail

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,148 +1,151 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
 
     fetch("components/header/header.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("header").innerHTML = data;
-    });
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+        });
 
 
     // Home
-    if(document.getElementById("hero")){
+    if (document.getElementById("hero")) {
 
         fetch("components/home/hero/hero.html")
-        .then(response => response.text())
-        .then(data=>{
-            document.getElementById("hero").innerHTML=data;
-        });
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("hero").innerHTML = data;
+            });
 
     }
 
 
-    if(document.getElementById("ourcourse")){
+    if (document.getElementById("ourcourse")) {
 
         fetch("components/home/ourcourse/ourcourse.html")
-        .then(response=>response.text())
-        .then(data=>{
-            document.getElementById("ourcourse").innerHTML=data;
-        });
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("ourcourse").innerHTML = data;
+            });
 
     }
 
 
-    if(document.getElementById("recommended")){
+    if (document.getElementById("recommended")) {
 
         fetch("components/home/recommended/recommended.html")
-        .then(response=>response.text())
-        .then(data=>{
-            document.getElementById("recommended").innerHTML=data;
-        });
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("recommended").innerHTML = data;
+            });
 
     }
 
 
-    if(document.getElementById("news")){
+    if (document.getElementById("news")) {
 
         fetch("components/home/news/news.html")
-        .then(response=>response.text())
-        .then(data=>{
-            document.getElementById("news").innerHTML=data;
-        });
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("news").innerHTML = data;
+            });
 
     }
 
 
-    if(document.getElementById("reviews")){
+    if (document.getElementById("reviews")) {
 
         fetch("components/home/reviews/reviews.html")
-        .then(response=>response.text())
-        .then(data=>{
-            document.getElementById("reviews").innerHTML=data;
-        });
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("reviews").innerHTML = data;
+            });
 
     }
 
 
     // ===== Short Course =====
 
-    if(document.getElementById("banner_overview")){
+    if (document.getElementById("banner_overview")) {
 
         fetch("components/short-course/banner_overview.html")
-        .then(response => response.text())
-        .then(data =>{
+            .then(response => response.text())
+            .then(data => {
 
-            document.getElementById("banner_overview").innerHTML = data;
+                document.getElementById("banner_overview").innerHTML = data;
 
-            // Swiper
-            if(document.querySelector(".highlightSwiper")){
+                // Swiper
+                if (document.querySelector(".highlightSwiper")) {
 
-                new Swiper(".highlightSwiper",{
-
-
-                    loop:false,
+                    new Swiper(".highlightSwiper", {
 
 
-                    centeredSlides:true,
+                        loop: false,
 
 
-                    slidesPerView:3,
+                        centeredSlides: true,
 
 
-                    spaceBetween:60,
+                        slidesPerView: 3,
 
 
-                    speed:600,
+                        spaceBetween: 60,
 
 
-                    grabCursor:true,
+                        speed: 600,
 
 
-
-                    navigation:{
-
-                        nextEl:".swiper-button-next",
-
-                        prevEl:".swiper-button-prev"
-
-                    },
+                        grabCursor: true,
 
 
 
-                    pagination:{
+                        navigation: {
 
-                        el:".swiper-pagination",
+                            nextEl: ".swiper-button-next",
 
-                        clickable:true
+                            prevEl: ".swiper-button-prev"
 
-                    },
-
-
-
-                    on:{
-
-
-                        slideChangeTransitionStart:function(){
-
-
-                            document
-                            .querySelectorAll(".highlight-card")
-                            .forEach(card=>{
-
-                                card.classList.remove("active");
-
-                            });
+                        },
 
 
 
-                            let active =
-                            this.slides[this.activeIndex]
-                            .querySelector(".highlight-card");
+                        pagination: {
+
+                            el: ".swiper-pagination",
+
+                            clickable: true
+
+                        },
 
 
-                            if(active){
 
-                                active.classList.add("active");
+                        on: {
+
+
+                            slideChangeTransitionStart: function () {
+
+
+                                document
+                                    .querySelectorAll(".highlight-card")
+                                    .forEach(card => {
+
+                                        card.classList.remove("active");
+
+                                    });
+
+
+
+                                let active =
+                                    this.slides[this.activeIndex]
+                                        .querySelector(".highlight-card");
+
+
+                                if (active) {
+
+                                    active.classList.add("active");
+
+                                }
+
 
                             }
 
@@ -150,79 +153,76 @@ document.addEventListener("DOMContentLoaded", function(){
                         }
 
 
-                    }
+                    });
 
+                }
 
-                });
-
-            }
-
-        });
+            });
 
     }
 
 
     // Course Curriculum
 
-    if(document.getElementById("course_curriculum")){
+    if (document.getElementById("course_curriculum")) {
 
         fetch("components/short-course/course-curriculum/course-curriculum.html")
 
-        .then(response => response.text())
-
-        .then(data => {
-
-
-            document.getElementById("course_curriculum").innerHTML = data;
-
-
-
-            // โหลด Course Card หลังจากมี courseGrid แล้ว
-
-            fetch("components/short-course/course-curriculum/course-card.html")
-
             .then(response => response.text())
 
-            .then(card => {
+            .then(data => {
 
 
-                let html = "";
+                document.getElementById("course_curriculum").innerHTML = data;
 
 
-                for(let i = 0; i < 16; i++){
 
-                    html += card;
+                // โหลด Course Card หลังจากมี courseGrid แล้ว
 
-                }
+                fetch("components/short-course/course-curriculum/course-card.html")
+
+                    .then(response => response.text())
+
+                    .then(card => {
 
 
-                document.getElementById("courseGrid").innerHTML = html;
+                        let html = "";
+
+
+                        for (let i = 0; i < 16; i++) {
+
+                            html += card;
+
+                        }
+
+
+                        document.getElementById("courseGrid").innerHTML = html;
+
+
+                    });
 
 
             });
-
-
-        });
 
     }
 
 
     // Curriculum Components
 
-    if(document.getElementById("curriculum_components")){
+    if (document.getElementById("curriculum_components")) {
 
 
         fetch("components/short-course/curriculum-components/curriculum-components.html")
 
-        .then(response=>response.text())
+            .then(response => response.text())
 
-        .then(data=>{
-
-
-            document.getElementById("curriculum_components").innerHTML = data;
+            .then(data => {
 
 
-        });
+                document.getElementById("curriculum_components").innerHTML = data;
+
+
+            });
 
 
     }
@@ -230,37 +230,37 @@ document.addEventListener("DOMContentLoaded", function(){
 
     // Pathway Master
 
-    if(document.getElementById("pathway_master")){
+    if (document.getElementById("pathway_master")) {
 
         fetch("components/short-course/pathway-master/pathway-master.html")
 
-        .then(response => response.text())
+            .then(response => response.text())
 
-        .then(data => {
+            .then(data => {
 
-            document.getElementById("pathway_master").innerHTML = data;
+                document.getElementById("pathway_master").innerHTML = data;
 
-        });
+            });
 
     }
 
 
     // Curriculum Structure
 
-    if(document.getElementById("curriculum_structure")){
+    if (document.getElementById("curriculum_structure")) {
 
 
         fetch("components/short-course/curriculum-structure/curriculum-structure.html")
 
-        .then(response=>response.text())
+            .then(response => response.text())
 
-        .then(data=>{
-
-
-            document.getElementById("curriculum_structure").innerHTML = data;
+            .then(data => {
 
 
-        });
+                document.getElementById("curriculum_structure").innerHTML = data;
+
+
+            });
 
 
     }
@@ -268,41 +268,350 @@ document.addEventListener("DOMContentLoaded", function(){
 
     // Total Program Cost
 
-    if(document.getElementById("total_program_cost")){
+    if (document.getElementById("total_program_cost")) {
 
         fetch("components/short-course/total-program-cost/total-program-cost.html")
 
-        .then(response => response.text())
+            .then(response => response.text())
 
-        .then(data =>{
+            .then(data => {
 
-            document.getElementById("total_program_cost").innerHTML = data;
+                document.getElementById("total_program_cost").innerHTML = data;
 
-        });
+            });
 
     }
 
 
+    // ===== Course Detail =====
+
+    if (document.getElementById("course_detail_banner")) {
+
+        fetch("components/course-detail/banner/banner.html")
+            .then(response => response.text())
+            .then(data => {
+
+                document.getElementById("course_detail_banner").innerHTML = data;
+
+            });
+
+    }
+
+
+    // Navbar course detail
+
+    if (document.getElementById("course_detail_navbar")) {
+
+
+        fetch("components/course-detail/navbar/navbar.html")
+
+            .then(res => res.text())
+
+            .then(data => {
+
+                document
+                    .getElementById("course_detail_navbar")
+                    .innerHTML = data;
+
+
+            });
+
+
+    }
+
+
+    // Course Detail Description
+
+    if (document.getElementById("description")) {
+
+
+        fetch("components/course-detail/description/description.html")
+
+            .then(res => res.text())
+
+            .then(data => {
+
+                document
+                    .getElementById("description")
+                    .innerHTML = data;
+
+            });
+
+
+    }
+
+
+    if (document.getElementById("content")) {
+
+
+        fetch("components/course-detail/content/content.html")
+
+
+            .then(res => res.text())
+
+
+            .then(data => {
+
+
+                document
+                    .getElementById("content")
+                    .innerHTML = data;
+
+
+
+                const lessons =
+                    document.querySelectorAll(".lesson-group");
+
+
+
+                lessons.forEach(group => {
+
+
+
+                    const title =
+                        group.querySelector(".lesson-title");
+
+
+
+                    const detail =
+                        group.querySelectorAll(".lesson-detail");
+
+
+
+                    const icon =
+                        title.querySelector("i");
+
+
+                    title.addEventListener("click", () => {
+
+                        detail.forEach(item => {
+
+
+
+                            if (item.style.display === "none") {
+
+
+                                item.style.display = "flex";
+
+
+                            }
+
+                            else {
+
+
+                                item.style.display = "none";
+
+
+                            }
+
+
+                        });
+
+                        if (icon.classList.contains("fa-chevron-up")) {
+
+                            icon.classList.remove(
+                                "fa-chevron-up"
+                            );
+
+                            icon.classList.add(
+                                "fa-chevron-down"
+                            );
+
+                        }
+
+                        else {
+
+                            icon.classList.remove(
+                                "fa-chevron-down"
+                            );
+
+                            icon.classList.add(
+                                "fa-chevron-up"
+                            );
+
+                        }
+
+                    });
+
+                });
+
+                // ==========================
+                // Hide All Content
+                // ==========================
+                document
+                    .getElementById("hideContent")
+                    .addEventListener("click", () => {
+
+
+
+                        document
+                            .querySelectorAll(".lesson-detail")
+                            .forEach(item => {
+
+
+                                item.style.display = "none";
+
+
+                            });
+
+                        document
+                            .querySelectorAll(".lesson-title i")
+                            .forEach(icon => {
+
+
+
+                                icon.classList.remove(
+                                    "fa-chevron-up"
+                                );
+
+
+                                icon.classList.add(
+                                    "fa-chevron-down"
+                                );
+
+                            });
+                    });
+
+            });
+
+    }
+
+
+    if (document.getElementById("instructor")) {
+
+        fetch("components/course-detail/instructor/instructor.html")
+
+            .then(res => res.text())
+
+            .then(data => {
+
+                document.getElementById("instructor").innerHTML = data;
+
+            });
+
+    }
+
+
+
+    // Certificate
+
+    if (document.getElementById("certificate")) {
+
+
+        fetch("components/course-detail/certificate/certificate.html")
+
+            .then(res => res.text())
+
+            .then(data => {
+
+                document.getElementById("certificate").innerHTML = data;
+
+            });
+
+
+    }
+
+
+    if (document.getElementById("reviews")) {
+
+
+        fetch("components/course-detail/reviews/reviews.html")
+
+            .then(res => res.text())
+
+            .then(data => {
+
+                document.getElementById("reviews").innerHTML = data;
+
+            });
+
+
+    }
+
+
+    if (document.getElementById("payment")) {
+
+
+        fetch("components/course-detail/payment/payment.html")
+
+            .then(res => res.text())
+
+            .then(data => {
+
+
+                document.getElementById("payment").innerHTML = data;
+
+
+
+                const items =
+                    document.querySelectorAll(".payment-item");
+
+
+
+                items.forEach(item => {
+
+
+                    const question =
+                        item.querySelector(".payment-question");
+
+
+                    question.addEventListener("click", () => {
+
+
+                        items.forEach(other => {
+
+
+                            if (other !== item) {
+
+                                other.classList.remove("active");
+
+                            }
+
+
+                        });
+
+
+
+                        item.classList.toggle("active");
+
+
+
+                    });
+
+
+                });
+
+
+
+            });
+
+
+    }
+
+
+    
+
+
     fetch("components/footer/footer.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("footer").innerHTML = data;
-    });
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
 
 
     fetch("components/report-problem/report-problem.html")
-    .then(res => res.text())
-    .then(data=>{
+        .then(res => res.text())
+        .then(data => {
 
-        document.body.insertAdjacentHTML(
-            "beforeend",
-            data
-        );
+            document.body.insertAdjacentHTML(
+                "beforeend",
+                data
+            );
 
-        document.dispatchEvent(
-            new Event("reportProblemLoaded")
-        );
+            document.dispatchEvent(
+                new Event("reportProblemLoaded")
+            );
 
-    });
+        });
 
 });
